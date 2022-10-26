f4e6c880ebab020582e146d7afa6ae8220f675ab
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2022-10-26 19:50:08 +0530

    changed file structure to one single page and created a basic gui

---
+++ app.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+gui = Tk()
+gui.title("Synapse")
+
+#set window size
+gui.geometry("1080x720+200+200")
+
+#set window colour
+gui.config(bg='white')
+
+gui.mainloop()
+
+
+
--- frontend/app.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append("..")
-from backend.req import *
-
-l = input("link : ")
-
-# link(l)
-get(l)
