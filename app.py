9f08ae71ac5fedb88782602ac3b7ae6935e8809e
KomalS2002 <komalsingh10072002@gmail.com>
2022-11-09 22:41:28 +0530

    response area and scrollbar added

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ buttonsframe = Frame(responseframe, padx = 30, pady = 30, bg = "white", highligh
 buttonsframe.pack(fill = "x")
 bframe = Frame(fr1, padx = 30, pady = 30, bg = "white",highlightbackground= "#1338be", highlightthickness=3)
 bframe.pack(anchor  ="center")
-
+statusframe = Frame(buttonsframe, bg="#fff", padx=30, pady=30, )
+statusframe.pack()
 #function 
 #to get which requested setted at request
 
@@ -98,16 +99,29 @@ sendButton = Button(fr2, text = "Send", command = requested, padx=18, pady=15, b
 sendButton.grid(row = 0, column = 2)
 saveButton = Button(fr2, text = "Save", command = download, padx=18, pady=15, bg="#1338be",fg="white")
 saveButton.grid(row=0,column=4 )
+tabControl= ttk.Notebook(statusframe)
+statusframe.grid(row=3, column=4)
+tab_4 = Label(tabControl,text ="", bg="#fff")
+
 # tabs widget
-# scrollbar = Scrollbar(tabControl)
-# scrollbar.pack( side = RIGHT, fill = Y )
-tabControl = ttk.Notebook (responseframe, height = 700, width = 1500  )
+scrollbar = Scrollbar(responseframe)
+scrollbar.pack( side = RIGHT, fill = Y )
+tabControl = ttk.Notebook (responseframe, height = 700, width = 1500)
 tab_1 = Label(tabControl, text="",bg="#fff")
 tab_2 = Label(tabControl, text="",bg="#fff")
 tab_3 = Label(tabControl, text="",bg="#fff")
 tabControl.add(tab_1, text = 'Raw',)
 tabControl.add(tab_2, text = 'Preview')
 tabControl.add(tab_3, text = 'Headers')
+
+
+
+# tabControl= ttk.Notebook(statusframe,height=40, width=80)
+# tabControl.grid(statusframe, row=3, column=4)
+
+# tabControl = ttk.Notebook(responseframe, 
+#                  yscrollcommand = scrollbar.set )
+# scrollbar.config( command = tabControl.yview )
 # add an output area
 # tabControl1 = ttk.Notebook(responseframe, height=40,width=50)
 # tab_4 = Label(tabControl1, text="", bg="#fff")
@@ -121,5 +135,6 @@ lbl2 = HTMLLabel(tab_2, html = "<h1>Click Send to get a response<h1/><br><h2>hel
 lbl2.pack()
 lbl3 = Label(tab_3, text = "Click Send to get a response", height=1150, width=1300, padx = 50, pady=50, bg = "#fff", fg = "black", font = "Helvetica 13")
 lbl3.pack()
-
+# lbl4 = Label(tab_4, text = "",height=40, width=1300, padx = 50, pady=50, bg = "#fff", fg = "black", font = "Helvetica 13")
+# lbl4.pack()
 frame.mainloop()
