f5a309a3d955c5183da9fe144684a729b8057d26
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2022-11-12 16:24:39 +0530

    change download function. Now user can download hfull or partial page of the g given url

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 from tkhtmlview import HTMLLabel
 from req import *
 import os
 from tkinter.simpledialog import askstring
 from bs4 import BeautifulSoup as bs
+from pywebcopy import save_website
 
 frame = Tk()
 frame.title("Synapse")
@@ -27,14 +29,16 @@ bframe = Frame(fr1, padx = 30, pady = 30, bg = "white",highlightbackground= "#13
 bframe.pack(anchor  ="center")
 statusframe = Frame(buttonsframe, bg="#fff", padx=30, pady=30, )
 statusframe.pack()
-#function 
-#to get which requested setted at request
 
+#function 
+
+#to get which requested setted at request
 def got():
     slktd = clicked.get()
     law = slktd.lower()
-    return(law)
+    return(law)   
 
+# http request function
 def requested():
     url = inputtxt.get(1.0, "end-1c")
     param = tab1.get(1.0, "end-1c")
@@ -47,15 +51,14 @@ def requested():
     prettyHTML = soup.prettify()
     lbl1.config(text = prettyHTML)
     lbl2.set_html(txt)
-    lbl3.config(text = txt)
-    return txt
+    lbl3.config(text = hed)
+    return url
 
+# Download and save function
 def download():
-    save = requested()
-    filename = askstring('Save File', 'Enter filename') + '.html'
-    myfile = open(filename, 'w')
-    myfile.write(save)
-    myfile.close()
+    lnk = requested()
+    save_website( url=lnk, project_folder="./saved_website/")
+    messagebox.showinfo("Success!", "Project folder saved.")
     
 options = [
     "GET",
@@ -65,11 +68,12 @@ options = [
     "DELETE",
 ]
 
-
 # datatype of menu text
 clicked = StringVar()
+
 # initial menu text
 clicked.set( "GET" )
+
 # Create Dropdown menu
 drop = OptionMenu( fr2 , clicked , *options, command=got)
 drop.grid(row = 0, column = 0)
@@ -81,7 +85,6 @@ inputtxt.grid(row = 0, column = 1, padx = 45)
 
 
 # tabs widget
-
 tabControl = ttk.Notebook( fr1,  height = 100, width = 1700 )
 tab1 = Text(tabControl,width=1000)
 tab2 = Text(tabControl,width=1000)
@@ -101,11 +104,8 @@ saveButton = Button(fr2, text = "Save", command = download, padx=18, pady=15, bg
 saveButton.grid(row=0,column=4 )
 tabControl= ttk.Notebook(statusframe)
 statusframe.grid(row=3, column=4)
-tab_4 = Label(tabControl,text ="", bg="#fff")
 
 # tabs widget
-scrollbar = Scrollbar(responseframe)
-scrollbar.pack( side = RIGHT, fill = Y )
 tabControl = ttk.Notebook (responseframe, height = 700, width = 1500)
 tab_1 = Label(tabControl, text="",bg="#fff")
 tab_2 = Label(tabControl, text="",bg="#fff")
@@ -114,18 +114,6 @@ tabControl.add(tab_1, text = 'Raw',)
 tabControl.add(tab_2, text = 'Preview')
 tabControl.add(tab_3, text = 'Headers')
 
-
-
-# tabControl= ttk.Notebook(statusframe,height=40, width=80)
-# tabControl.grid(statusframe, row=3, column=4)
-
-# tabControl = ttk.Notebook(responseframe, 
-#                  yscrollcommand = scrollbar.set )
-# scrollbar.config( command = tabControl.yview )
-# add an output area
-# tabControl1 = ttk.Notebook(responseframe, height=40,width=50)
-# tab_4 = Label(tabControl1, text="", bg="#fff")
-# tabControl.add(tab_4, text = 'Response')
 tabControl.pack(fill = "both")
 
 # Label Creation
@@ -135,6 +123,5 @@ lbl2 = HTMLLabel(tab_2, html = "<h1>Click Send to get a response<h1/><br><h2>hel
 lbl2.pack()
 lbl3 = Label(tab_3, text = "Click Send to get a response", height=1150, width=1300, padx = 50, pady=50, bg = "#fff", fg = "black", font = "Helvetica 13")
 lbl3.pack()
-# lbl4 = Label(tab_4, text = "",height=40, width=1300, padx = 50, pady=50, bg = "#fff", fg = "black", font = "Helvetica 13")
-# lbl4.pack()
+
 frame.mainloop()
